0e5d2ccaf2ec4071a423dc44235141c74314cdfa
sonalieva <708v8lg@code.edu.az>
2022-04-06 09:54:45 +0400

    created

---
+++ 6aprel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ClassLibrary;
+
+namespace _6aprel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name = "Cola";
+            product.Price = 1.8;
+            product.Type = ProductType.Drink;
+            Product product1 = new Product();
+            product1.Name = "Pork";
+            product1.Price = 8;
+            product1.Type = ProductType.Meat;
+            Store smth = new Store();
+            smth.AddProduct(product);
+            smth.AddProduct(product1);
+            foreach (var item in smth.Products)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            foreach (var item in smth.FilterProductByName("Meat"))
+            {
+                Console.WriteLine(item.Name);
+            }
+            foreach (var item in smth.FilterProductsByType(ProductType.Drink))
+            {
+                Console.WriteLine(item.Name);
+            }
+            
+        }
+    }
+}
---
+++ ClassLibrary/Store.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Store
+    {
+        Product[] products = new Product[0];
+        public Product[] Products { get { return products; } }
+        public void AddProduct(Product product)
+        {
+            Array.Resize(ref products, products.Length + 1);
+            products[products.Length - 1] = product;
+        }
+        public Product[] FilterProductsByType(ProductType type)
+        {
+            Product[] filteredProducts = new Product[0];
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (type == products[i].Type)
+                {
+                    Array.Resize(ref filteredProducts, filteredProducts.Length + 1);
+                    filteredProducts[filteredProducts.Length - 1] = products[i];
+                }
+            }
+            return filteredProducts;
+        }
+        public Product[] FilterProductByName(string name)
+        {
+            Product[] filteredProducts = new Product[0];
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i].Name == name)
+                {
+                    Array.Resize(ref filteredProducts, filteredProducts.Length + 1);
+                    filteredProducts[filteredProducts.Length - 1] = products[i];
+                }
+            }
+            return filteredProducts;
+        }
+    }
+}
